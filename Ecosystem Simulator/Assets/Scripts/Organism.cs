a213e969462fb2df5d1cdfe477a33a3ac26e07b6
Evan Combs <evan@combs-family.com>
2021-02-11 20:52:41 -0500

    Added basic constructors to Organism class, started an Organism driver to interface with Unity

--- Ecosystem Simulator/Assets/Scripts/Organism.cs
+++ Ecosystem Simulator/Assets/Scripts/Organism.cs
@@ -9,7 +9,26 @@ public class Organism
     private int currentHealth;
 
     private int nutritionValue;
+
+    private int age;
     // Add model 
 
     // Add common organism functions, kill(), grow() ? etc.
+
+    // Organisms are not MonoBehavior objects; OrganismUpdate() should be called on every 
+    // gametick by Unity Update() function
+    public void OrganismUpdate()
+    {
+
+    }
+    public Organism()
+    {
+
+    }
+    public Organism(int maxHealth, int nutritionValue)
+    {
+        this.age = 0;
+        this.maxHealth = maxHealth;
+        this.currentHealth = maxHealth;
+    }
 }
---
+++ Ecosystem Simulator/Assets/Scripts/OrganismDriver.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrganismDriver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Organism organism;
+    public int test;
+    void Start()
+    {
+        // organism = new Predator();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // organism.OrganismUpdate();
+    }
+}
