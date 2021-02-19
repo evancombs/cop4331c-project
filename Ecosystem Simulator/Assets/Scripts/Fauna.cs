1ad1647bc33b9efdd6121b8d0beaa1f7545932fe
Evan Combs <evan@combs-family.com>
2021-02-19 13:35:01 -0500

    Completely reworked class structure Changed organism structure to be more object oriented Switched to organism driver system Removed old water, prey, flora, predator scripts

---
+++ Ecosystem Simulator/Assets/Scripts/Fauna.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fauna : Organism
+{
+    double movementSpeed;
+    int nutritionLevel;
+}
---
+++ Ecosystem Simulator/Assets/Scripts/Organism.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Organism
+{
+    int waterLevel;
+    int nutrientValue;
+
+    double reproductiveChance;
+    int reproductiveRate;
+
+}
