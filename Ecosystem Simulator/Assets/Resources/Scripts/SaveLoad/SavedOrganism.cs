d78455f02c45eb1e8841e2f4a2f1b088533e59bc
Evan Combs <evan@combs-family.com>
2021-03-19 17:53:12 -0400

    [U016],[U006] Adding Serializable Scripts to save states to files

---
+++ Ecosystem Simulator/Assets/Resources/Scripts/SaveLoad/SavedOrganism.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SavedOrganism 
+{
+    public float waterLevel;
+    public float nutrientValue;
+
+    public double reproductiveChance;
+    public int reproductiveRate;
+
+    public double awareness = 10;
+
+}
