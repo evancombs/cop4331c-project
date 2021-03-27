c607d28f61eeb8bb31cdc0e25d4aacef694950c7
Evan Combs <evan@combs-family.com>
2021-03-26 20:10:41 -0400

    [U006]

--- Ecosystem Simulator/Assets/Resources/Scripts/SaveLoad/SavedFaunaNutrient.cs
+++ Ecosystem Simulator/Assets/Resources/Scripts/SaveLoad/SavedFaunaNutrient.cs
@@ -4,8 +4,31 @@ using UnityEngine;
 
 public class SavedFaunaNutrient : SavedNutrient
 {
-    // public static GameObject LoadFaunaNutrient()
-    //{
+     public static GameObject loadNutrient(SavedFaunaNutrient faunaNutrientSave)
+    {
+        GameObject loadedNutrient = GameObject.CreatePrimitive(PrimitiveType.Cube);
 
-    //}
+        FaunaNutrient nutrientscriptRef = loadedNutrient.AddComponent<FaunaNutrient>();
+        loadedNutrient.GetComponent<MeshRenderer>().material = (Resources.Load("Materials/FaunaNutrient", typeof(Material)) as Material);
+
+        loadedNutrient.transform.localScale = new Vector3(1f, 1f, 1f);
+        loadedNutrient.transform.position = faunaNutrientSave.savedTransform.position;
+
+        nutrientscriptRef.remainingNutrients = faunaNutrientSave.remainingNutrients;
+
+        return loadedNutrient;
+    }
+
+    public static SavedFaunaNutrient SaveNutrient(GameObject nutrientObject)
+    {
+        FaunaNutrient nutrientscriptRef = nutrientObject.GetComponent<FaunaNutrient>();
+        SavedFaunaNutrient nutrientSave = new SavedFaunaNutrient();
+
+// loadedNutrient.transform.position = faunaNutrientSave.savedTransform.position;
+
+  //      nutrientscriptRef.remainingNutrients = faunaNutrientSave.remainingNutrients;
+
+        return null;
+    }
+
 }
